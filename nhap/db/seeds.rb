3ef890ea249249cbab4b5dde55392c8794867a37
phuquoc <votruongtoan123@gmail.com>
2016-11-23 15:33:03 +0700

    Blog Version 2
    Blog Version 2

---
+++ nhap/db/seeds.rb
@@ -0,0 +1,10 @@
+Blog.delete_all
+Comment.delete_all
+article = Blog.create!(title: 'test', content: 'for test')
+
+Blog.create!(title: 'test', content: 'for test')
+Blog.create!(title: 'test2', content: 'for test2')
+Comment.create!(name: 'quoc', content: 'phu quoc commented',
+blog_id: article.id)
+Comment.create!(name: 'Nho', content: 'Phan Anh commented',
+blog_id: article.id)
